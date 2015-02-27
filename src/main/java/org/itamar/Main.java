d5f0e6781e262209de186a6d755e0a250c0e17f1
Re'em Bensimhon <reem@forter.com>
2015-02-27 19:33:03 +0200

    Added instructions for first assignment

--- src/main/java/org/itamar/Main.java
+++ src/main/java/org/itamar/Main.java
@@ -4,14 +4,31 @@ import java.util.Scanner;
 
 /**
  * This is the pipeline main class
- * b *
- * c %
- * f &
- * d !
- * o @
- * m ^
+ *
+ *
+ *
+ *
+ *
+ *
  */
 public class Main {
+    /**
+
+     First assignment:
+       - Add new steps to the pipeline that will replace the following characters in the strings:
+
+        b *
+        c %
+        f &
+        d !
+        o @
+        m ^
+
+        In this assignment you should change replaceAs to ne called replaceAll and get, as well as the input, the char it
+        needs to replace and the replacement char to replace with.
+     */
+
+
     static Scanner reader = new Scanner(System.in);
 
     public static void main(String[] args) {
